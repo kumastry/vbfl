d01b7ab79a5346dbf9b91a84f2b81369f431592b
yu2799 <ura.yu.2799@gmail.com>
2021-07-02 03:01:27 +0900

    add a page for pre-test settings

---
+++ src/components/Ready.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { IonPage, IonToggle, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonLabel, IonItem } from '@ionic/react';
+
+const Settings = () => {
+  const [four, setFour] = useState(false); // toggleをdisableにするためのstate
+  return(
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>unko</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+            <IonToolbar>
+                <IonTitle size="large">unko</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        {/*<ExploreContainer name="Tab 2 page" />*/}
+        <IonCard>
+          <IonItem>
+            <IonLabel>
+                <h2>ランダムに出題</h2>
+                <p>単語帳の並びをランダムにします。</p>    
+            </IonLabel>
+            <IonToggle value="randam" value="randam" />
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+                <h2>問題を四択にする</h2>
+                <p>入力形式から選択形式にします。</p>
+            </IonLabel>
+            <IonToggle value={four} onIonChange={e => setFour(e.detail.checked)}/>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+                <h2>strictモードにする</h2>
+                <p>文字列が完全に一致しているとき正解になります。</p>
+            </IonLabel>
+            <IonToggle disabled={four} value="strict" />
+          </IonItem>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+}
+
+export default Settings;
