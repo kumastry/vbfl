ce069f19f35e07d6d7cd11e4e1f593e6cdb88552
yu2799 <ura.yu.2799@gmail.com>
2021-07-16 09:14:55 +0900

    indent

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,72 +1,67 @@
-import { IonContent, IonList, IonPage, IonAlert, IonItem} from '@ionic/react';
-import Addbutton from '../components/Addbutton';
-import Card from '../components/Card';
-import Header from '../components/Header';
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
+import { IonContent, IonList, IonPage, IonAlert, IonItem } from "@ionic/react";
+import Addbutton from "../components/Addbutton";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
-import { addCard } from '../slices/cardSlice';
-import CardsList from '../components/CardsList';
+import { addCard } from "../slices/cardSlice";
+import CardsList from "../components/CardsList";
 
 const MainPage = () => {
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
-  
 
   const dispatchCard = (title) => {
     console.log("FFF");
-    
+
     if (title.length > 0) {
       console.log(title);
       dispatch(
         addCard({
-            id: nanoid(),
-            title,
-            content:[],
-          })
-      )
+          id: nanoid(),
+          title,
+          content: [],
+        })
+      );
     }
-  }
+  };
 
-  const handleClick= () => {
+  const handleClick = () => {
     setShowModal(true);
-  }
-
+  };
 
   return (
     <IonPage>
-      <Header/>
+      <Header />
       <IonContent>
+        <CardsList />
 
-      
-
-        <CardsList/>
-
-        
-        <IonAlert 
-        isOpen = {showModal} 
-        onDidDismiss={() => setShowModal(false)}
-        cssClass='my-custom-class'
-        header={'単語帳を新たに追加'}
-      
-        inputs = { [{
-          name:'name',
-          type:'text',
-          placeholder: '名前'
-        }] }
-        
-        buttons={[{
-          text:'キャンセル',
-          role: 'cancel'
-        },
-        {
-          text:'OK',
-          handler: data => dispatchCard(data.name)
-        } ]}
+        <IonAlert
+          isOpen={showModal}
+          onDidDismiss={() => setShowModal(false)}
+          cssClass="my-custom-class"
+          header={"単語帳を新たに追加"}
+          inputs={[
+            {
+              name: "name",
+              type: "text",
+              placeholder: "名前",
+            },
+          ]}
+          buttons={[
+            {
+              text: "キャンセル",
+              role: "cancel",
+            },
+            {
+              text: "OK",
+              handler: (data) => dispatchCard(data.name),
+            },
+          ]}
         />
-        <Addbutton handleClick = {handleClick}/>
-
+        <Addbutton handleClick={handleClick} />
       </IonContent>
     </IonPage>
   );
