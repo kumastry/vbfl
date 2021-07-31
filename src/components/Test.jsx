ffc537f2541a09cb146d5e56a6e8c664030ffd09
yu2799 <ura.yu.2799@gmail.com>
2021-07-31 18:50:14 +0900

    戻るボタン変更．alertで本当にやめるか聞くのを追加．

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -15,10 +15,14 @@ import {
   IonCardContent,
   IonProgressBar,
   IonButtons,
-  IonBackButton,
+  IonButton,
+  IonIcon,
+  IonAlert,
 } from "@ionic/react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { chevronBack } from "ionicons/icons";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Test = ({ match }) => {
   // localStorage.clear();
@@ -26,6 +30,10 @@ const Test = ({ match }) => {
   const Id = match.params.cardId;
   const WS = useSelector((state) => state.cards.card);
   const Words = WS.find((data) => data.id === Id);
+
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory();
+  const handleLink = (path) => history.push(path);
 
   const wordCardsLength = Words.content.length;
   const reversed = false;
@@ -36,7 +44,14 @@ const Test = ({ match }) => {
         <IonToolbar>
           <IonTitle>test</IonTitle>
           <IonButtons slot="start">
-            <IonBackButton defaultHref="/tab1" />
+            <IonButton
+              type="button"
+              color="danger"
+              onClick={() => setShowModal(true)}
+            >
+              <IonIcon icon={chevronBack} />
+              やめる
+            </IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -93,6 +108,22 @@ const Test = ({ match }) => {
           );
         })}
         <IonProgressBar value={1}></IonProgressBar>
+        <IonAlert
+          isOpen={showModal}
+          onDidDismiss={() => setShowModal(false)}
+          cssClass="my-custom-class"
+          message={"今回の結果は保存されません。本当にやめますか？"}
+          buttons={[
+            {
+              text: "キャンセル",
+              role: "cancel",
+            },
+            {
+              text: "OK",
+              handler: () => handleLink("/"),
+            },
+          ]}
+        />
       </IonContent>
     </IonPage>
   );
