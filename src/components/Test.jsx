615e7721b4213167ee9d69cb0b51bc26974bb79b
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-07-31 23:09:13 +0900

    quiz fixed

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -17,9 +17,36 @@ import {
   IonButtons,
   IonBackButton,
 } from "@ionic/react";
-import { useState } from "react";
+import { shuffle } from "ionicons/icons";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
+
+const shuffleArray = ([...array]) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const removeCorrect = ([...array], correctId) => {
+  let removeId = -1;
+  console.log(array);
+  console.log(correctId);
+  for(let i = 0; i < array.length; i++) {
+    if(array[i]['translateId'] === correctId) {
+      removeId =i;
+      break;
+    }
+  }
+
+  console.log(removeId);
+  array.splice(removeId, 1);
+
+  return array;
+}
 
 const Test = ({ match }) => {
   // localStorage.clear();
@@ -28,21 +55,54 @@ const Test = ({ match }) => {
   const WS = useSelector((state) => state.cards.card);
   const Words = WS.find((data) => data.id === Id);
   const [curId, setcurId] = useState(0);
+  const [alter, setAlter] = useState([]);
   const wordCardsLength = Words.content.length;
 
-  const word = [];
-  const translate = [];
-
+  const words = [];
+  let translates = [];
+  let translateId = 0;
   for (const item of Words.content) {
-    word.push(item.word)
-    translate.push(item.translate);
+    words.push({word:item.word, translate:item.translate, translateId});
+    translates.push({translateId, translate:item.translate});
+    translateId++;
   }
 
-  console.log(word);
-  console.log(translate);
+  const ClickHander = (inputId, currentId) => {
+    if(inputId === currentId) {
+      correctNumber ++;
+      alert("正解，正解数:"+correctNumber);
+      
+    } else {
+      alert("不正解")
+    }
+
+    setcurId(curId+1);
+  }
+  console.log(words);
+  console.log(translates);
 
   const reversed = false;
+  let correctNumber = 0;
 
+
+
+  useEffect(() => {
+    let alternative = [];
+    const transArray = removeCorrect(shuffleArray(translates), words[curId]['translateId']);
+    console.log(transArray);
+    alternative.push({translateId:words[curId]['translateId'], translate:words[curId]['translate']});
+    console.log("####");
+    console.log(alternative);
+    for(let i = 0; i < 3; i++) {
+      alternative.push({translateId:transArray[i]['translateId'], translate:transArray[i]['translate']});
+    }
+
+    alternative = shuffleArray(alternative);
+    setAlter(alternative);
+    console.log(alter);
+  }, [curId]);
+
+  
   return (
     <IonPage>
 
@@ -69,41 +129,46 @@ const Test = ({ match }) => {
                     {`${curId + 1} / ${wordCardsLength}`}
                   </IonCardSubtitle>
                   <IonCardTitle>
-                    {reversed ? translate[curId] : word[curId]}
+                    {curId >=  wordCardsLength? "正解数:"+correctNumber: words[curId]['word']}
                   </IonCardTitle>
                 </IonCardHeader>
               </IonCard>
+
               <IonGrid>
                 <IonRow>
+
                   <IonCol offset-4>
-                    <IonCard onClick = {() => setcurId(curId+1)}>
+                    <IonCard onClick = {() => ClickHander(alter[0]['translateId'], curId)}>
                       <IonLabel>1.</IonLabel>
-                      <IonCardContent>中身</IonCardContent>
+                      <IonCardContent>{alter.length === 0 ? "loading..." : alter[0]['translate']}</IonCardContent>
                     </IonCard>
                   </IonCol>
+
                   <IonCol offset-4>
-                    <IonCard>
+                    <IonCard onClick = {() => ClickHander(alter[1]['translateId'], curId)}>
                       <IonLabel>2.</IonLabel>
-                      <IonCardContent>中身</IonCardContent>
+                      <IonCardContent>{alter.length === 0 ? "loading..." : alter[1]['translate']}</IonCardContent>
                     </IonCard>
                   </IonCol>
                 </IonRow>
                 <IonRow>
                   <IonCol offset-4>
-                    <IonCard>
+                    <IonCard onClick = {() => ClickHander(alter[2]['translateId'], curId)}>
                       <IonLabel>3.</IonLabel>
-                      <IonCardContent>中身</IonCardContent>
+                      <IonCardContent>{alter.length === 0 ? "loading..." : alter[2]['translate']}</IonCardContent>
                     </IonCard>
                   </IonCol>
                   <IonCol offset-4>
-                    <IonCard>
+                    <IonCard onClick = {() => ClickHander(alter[3]['translateId'], curId)}>
                       <IonLabel>4.</IonLabel>
-                      <IonCardContent>中身</IonCardContent>
+                      <IonCardContent>{alter.length === 0 ? "loading..." : alter[3]['translate']}</IonCardContent>
                     </IonCard>
                   </IonCol>
                 </IonRow>
               </IonGrid>
+
             </IonCard>
+
         <IonProgressBar value={1}></IonProgressBar>
       </IonContent>
     </IonPage>
