502a5a77d45a639b9b802f127b8859c0f96f7c8e
yu2799 <ura.yu.2799@gmail.com>
2021-08-02 16:07:09 +0900

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-//import "bulma/css/bulma.css";
-import store from './app/store'
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import store from "./app/store";
+import { Provider } from "react-redux";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  
-    <Provider store = {store}>
-      <App />
-    </Provider> 
-  ,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
