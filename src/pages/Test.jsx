9cccd3b6c4c1e0930264dd5219ea31318f1dca21
yu2799 <ura.yu.2799@gmail.com>
2021-08-03 17:07:38 +0900

    add select reverse

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -8,17 +8,14 @@ import {
   IonRow,
   IonCol,
   IonCard,
-  IonLabel,
   IonCardSubtitle,
   IonCardTitle,
   IonCardHeader,
-  IonCardContent,
   IonProgressBar,
   IonButtons,
   IonButton,
   IonIcon,
   IonAlert,
-  IonItem,
   IonInput,
 } from "@ionic/react";
 import { useState, useEffect } from "react";
@@ -28,6 +25,7 @@ import { useHistory } from "react-router";
 import { totalCollectCountUp } from "../slices/achievementSlice";
 import { PieChart, Pie, ResponsiveContainer, Cell } from "recharts";
 
+// goooooooooooooooood!!!!!!
 const shuffleArray = ([...array]) => {
   for (let i = array.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -36,49 +34,40 @@ const shuffleArray = ([...array]) => {
   return array;
 };
 
-const removeCorrect = ([...array], correctId) => {
-  let removeId = -1;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i]["translateId"] === correctId) {
-      removeId = i;
-      break;
-    }
-  }
-  array.splice(removeId, 1);
-  return array;
+// 単語解答後の処理
+const switchWord = (setShowAlert, setCurId, curId) => {
+  setShowAlert(true);
+  setTimeout(() => {
+    setShowAlert(false);
+    setCurId(curId + 1);
+  }, 1000);
 };
 
 const Test = ({ match }) => {
   // localStorage.clear();
-  // redux系準備
   const [showAlert1, setShowAlert1] = useState(false);
   const [showAlert2, setShowAlert2] = useState(false);
   const [showAlert3, setShowAlert3] = useState(false);
+
   const Id = match.params.cardId;
   const WS = useSelector((state) => state.cards.card);
   const Words = WS.find((data) => data.id === Id);
   const dispatch = useDispatch();
 
   const [showModal, setShowModal] = useState(false);
-  const history = useHistory();
   const [curId, setcurId] = useState(0);
   const [correct, setCorrect] = useState(0);
-  const [alter, setAlter] = useState([]);
   const wordCardsLength = Words.content.length;
-  const words = [];
-  let translates = [];
-  let translateId = 0;
+
+  const [answerSet, setAnswerSet] = useState([]);
+  const [wordsSet, setWordsSet] = useState(Words.content);
+  const [selectSet, setSelectSet] = useState([]);
 
   const [data, setData] = useState([]);
   const [inputText, setInputText] = useState("");
 
+  const history = useHistory();
   const handleLink = (path) => history.push(path);
-
-  for (const item of Words.content) {
-    words.push({ word: item.word, translate: item.translate, translateId });
-    translates.push({ translateId, translate: item.translate });
-    translateId++;
-  }
 
   const AnswerAlert = ({ alert, msg }) => {
     return <IonAlert isOpen={alert} header={"結果"} message={msg} />;
@@ -86,82 +75,67 @@ const Test = ({ match }) => {
 
   const fourex = () => {
     setShowAlert3(false);
-    window.location.href=`ready/${Id}`;
-  }
+    window.location.href = `ready/${Id}`;
+  };
 
-  const fourClickHander = (inputId, currentId) => {
-    if (inputId === currentId) {
+  const fourClickHander = (nowText, selectText) => {
+    if (nowText === selectText) {
       setCorrect(correct + 1);
-      setShowAlert1(true);
-      setTimeout(() => {
-        setShowAlert1(false);
-        setcurId(curId + 1);
-      }, 1000);
+      switchWord(setShowAlert1, setcurId, curId);
     } else {
-      setShowAlert2(true);
-      setTimeout(() => {
-        setShowAlert2(false);
-        setcurId(curId + 1);
-      }, 1000);
+      switchWord(setShowAlert2, setcurId, curId);
     }
   };
 
-  const inputClickHander = () => {
-    if (inputText === words[curId]["translate"]) {
-      setCorrect(correct + 1);
-      setShowAlert1(true);
-      setTimeout(() => {
-        setShowAlert1(false);
-        setcurId(curId + 1);
-      }, 1000);
-    } else {
-      setShowAlert2(true);
-      setTimeout(() => {
-        setShowAlert2(false);
-        setcurId(curId + 1);
-      }, 1000);
-    }
+  const inputClickHander = (nowText) => {
     setInputText("");
-    setTimeout(() => {
-      setShowAlert1(false);
-      setcurId(curId + 1);
-    }, 1000);
+    if (inputText === nowText) {
+      setCorrect(correct + 1);
+      switchWord(setShowAlert1, setcurId, curId);
+    } else {
+      switchWord(setShowAlert2, setcurId, curId);
+    }
   };
+
+  // 初回のみrandomがtrueなら単語帳をシャッフル
+  // 初回のみreverseがtureなら裏側を選択肢リストに入れる
+  useEffect(() => {
+    if (Words.random) {
+      setWordsSet((prev) => shuffleArray(prev));
+    }
+    const tmp = [];
+    for (const { word, translate } of Words.content) {
+      tmp.push(Words.reverse ? word : translate);
+    }
+    setAnswerSet((prev) => [...prev, ...tmp]);
+  }, []);
 
   useEffect(() => {
-    if(wordCardsLength < 4) {
+    if (wordCardsLength <= 3) {
       setShowAlert3(true);
     } else {
-    if (curId < wordCardsLength) {
-      if (Words.four) {
-        let alternative = [];
-        const transArray = removeCorrect(
-          shuffleArray(translates),
-          words[curId]["translateId"]
-        );
-        alternative.push({
-          translateId: words[curId]["translateId"],
-          translate: words[curId]["translate"],
-        });
-        for (let i = 0; i < 3; i++) {
-          alternative.push({
-            translateId: transArray[i]["translateId"],
-            translate: transArray[i]["translate"],
-          });
+      if (curId < wordCardsLength) {
+        if (Words.four) {
+          const targetIndex = Math.floor(Math.random() * 3);
+          const targetItem = Words.reverse
+            ? wordsSet[curId]["word"]
+            : wordsSet[curId]["translate"];
+          const newSelectSet = shuffleArray(
+            answerSet.filter((answer) => answer !== targetItem)
+          )
+            .slice(0, 4)
+            .map((item, index) => (index === targetIndex ? targetItem : item));
+          setSelectSet(newSelectSet);
         }
-
-        alternative = shuffleArray(alternative);
-        setAlter(alternative);
+      } else {
+        dispatch(totalCollectCountUp(correct));
+        setData([
+          { name: "Group A", value: correct },
+          { name: "Group B", value: wordCardsLength - correct },
+        ]);
       }
-    } else {
-      dispatch(totalCollectCountUp(correct));
-      setData([
-        { name: "Group A", value: correct },
-        { name: "Group B", value: wordCardsLength - correct },
-      ]);
     }
-  }
-  }, [curId]);
+  }, [curId, answerSet]);
 
   return (
     <IonPage>
@@ -172,7 +146,7 @@ const Test = ({ match }) => {
               <>
                 <IonHeader>
                   <IonToolbar>
-                    <IonTitle>result</IonTitle>
+                    <IonTitle>test</IonTitle>
                     <IonButtons slot="start">
                       <IonButton
                         type="button"
@@ -187,81 +161,49 @@ const Test = ({ match }) => {
                 </IonHeader>
 
                 <IonContent fullscreen>
-                  <IonCard key={curId}>
+                  <IonCard>
                     <IonCard>
                       <IonCardHeader>
                         <IonCardSubtitle>
                           {`${curId + 1} / ${wordCardsLength}`}
                         </IonCardSubtitle>
                         <IonCardTitle>
-                          {curId >= wordCardsLength
+                          {Words.reverse
+                            ? wordsSet[curId]["translate"]
+                            : wordsSet[curId]["word"]}
+                          {/* {curId >= wordCardsLength
                             ? `正解数:  ${correct}`
-                            : words[curId]["word"]}
+                            : words[curId]["word"]} */}
                         </IonCardTitle>
                       </IonCardHeader>
                     </IonCard>
 
                     <IonGrid>
                       <IonRow>
-                        <IonCol offset-4>
-                          <IonCard
-                            onClick={() =>
-                              fourClickHander(alter[0]["translateId"], curId)
-                            }
-                          >
-                            <IonLabel>1.</IonLabel>
-                            <IonCardContent>
-                              {alter.length === 0
-                                ? "loading..."
-                                : alter[0]["translate"]}
-                            </IonCardContent>
-                          </IonCard>
-                        </IonCol>
-
-                        <IonCol offset-4>
-                          <IonCard
-                            onClick={() =>
-                              fourClickHander(alter[1]["translateId"], curId)
-                            }
-                          >
-                            <IonLabel>2.</IonLabel>
-                            <IonCardContent>
-                              {alter.length === 0
-                                ? "loading..."
-                                : alter[1]["translate"]}
-                            </IonCardContent>
-                          </IonCard>
-                        </IonCol>
-                      </IonRow>
-                      <IonRow>
-                        <IonCol offset-4>
-                          <IonCard
-                            onClick={() =>
-                              fourClickHander(alter[2]["translateId"], curId)
-                            }
-                          >
-                            <IonLabel>3.</IonLabel>
-                            <IonCardContent>
-                              {alter.length === 0
-                                ? "loading..."
-                                : alter[2]["translate"]}
-                            </IonCardContent>
-                          </IonCard>
-                        </IonCol>
-                        <IonCol offset-4>
-                          <IonCard
-                            onClick={() =>
-                              fourClickHander(alter[3]["translateId"], curId)
-                            }
-                          >
-                            <IonLabel>4.</IonLabel>
-                            <IonCardContent>
-                              {alter.length === 0
-                                ? "loading..."
-                                : alter[3]["translate"]}
-                            </IonCardContent>
-                          </IonCard>
-                        </IonCol>
+                        {selectSet.map((data, index) => {
+                          // console.log(data);
+                          return (
+                            <IonCol key={index} size="6">
+                              <IonCard
+                                onClick={() =>
+                                  fourClickHander(
+                                    data,
+                                    Words.reverse
+                                      ? wordsSet[curId]["word"]
+                                      : wordsSet[curId]["translate"]
+                                  )
+                                }
+                              >
+                                <IonCardSubtitle
+                                  style={{ padding: "8px" }}
+                                >{` ${index + 1}.`}</IonCardSubtitle>
+                                <IonCardTitle style={{ padding: "8px" }}>
+                                  {answerSet.length === 0 ? "loading..." : data}
+                                </IonCardTitle>
+                              </IonCard>
+                            </IonCol>
+                          );
+                        })}
                       </IonRow>
                     </IonGrid>
                   </IonCard>
@@ -293,18 +235,18 @@ const Test = ({ match }) => {
                   />
 
                   <IonAlert
-                  isOpen={showAlert3}
-                  onDidDismiss = {fourex} 
-                  header={""} 
-                  message={"単語帳を4つ以上追加してください"}
-                  buttons= {[
-                    {
-                    text:"戻る",
-                    handler: () => {
-                      window.location.href = `ready/${Id}`;
-                    }
-                  }
-                  ]}
+                    isOpen={showAlert3}
+                    onDidDismiss={fourex}
+                    header={""}
+                    message={"単語帳を4つ以上追加してください"}
+                    buttons={[
+                      {
+                        text: "戻る",
+                        handler: () => {
+                          window.location.href = `ready/${Id}`;
+                        },
+                      },
+                    ]}
                   />
                 </IonContent>
               </>
@@ -329,11 +271,6 @@ const Test = ({ match }) => {
                 </IonHeader>
 
                 <IonContent fullscreen>
-                  <IonHeader collapse="condense">
-                    <IonToolbar>
-                      <IonTitle size="large">test</IonTitle>
-                    </IonToolbar>
-                  </IonHeader>
                   <IonGrid>
                     <IonRow>
                       <IonCol>
@@ -416,7 +353,9 @@ const Test = ({ match }) => {
                         <IonCardTitle>
                           {curId >= wordCardsLength
                             ? `正解数:  ${correct}`
-                            : words[curId]["word"]}
+                            : Words.reverse
+                            ? wordsSet[curId]["translate"]
+                            : wordsSet[curId]["word"]}
                         </IonCardTitle>
                       </IonCardHeader>
                     </IonCard>
@@ -456,14 +395,17 @@ const Test = ({ match }) => {
                     alert={showAlert2}
                     msg={`不正解... 正解数:${correct}`}
                   />
-
-                  <IonAlert
-                  />
                 </IonContent>
                 <IonButton
                   className="ion-margin"
                   expand="block"
-                  onClick={inputClickHander}
+                  onClick={() => {
+                    inputClickHander(
+                      Words.reverse
+                        ? wordsSet[curId]["word"]
+                        : wordsSet[curId]["translate"]
+                    );
+                  }}
                 >
                   awesome button!
                 </IonButton>
